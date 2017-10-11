d6eb80fb4de6f86a409ec920450871e00927d022
Pablo Holanda <phol4nda@gmail.com>
2017-10-11 11:43:50 -0300

    Removido parâmetro errado da função de leitura da porta digital

--- ler_porta_digital.py
+++ ler_porta_digital.py
@@ -10,7 +10,7 @@ start = time.time()
 PERIOD_OF_TIME = 1  # 120 min
 
 while True :
-    print com.ler_entrada(1, True)
+    print com.ler_entrada(1)
     if time.time() > start + PERIOD_OF_TIME: break
 
 com.fechar_conexao()
