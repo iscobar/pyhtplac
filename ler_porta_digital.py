fc054fc361bdf42f5e8f6b57f1faac6bccfba5df
Pablo Holanda <phol4nda@gmail.com>
2017-10-11 11:42:10 -0300

    Code para leitura da porta 1 durante 1 min

---
+++ ler_porta_digital.py
@@ -0,0 +1,16 @@
+from pyhtplac import *
+import time
+
+com = ComHtplac()
+
+print com.teste_conexao()
+
+start = time.time()
+
+PERIOD_OF_TIME = 1  # 120 min
+
+while True :
+    print com.ler_entrada(1, True)
+    if time.time() > start + PERIOD_OF_TIME: break
+
+com.fechar_conexao()
